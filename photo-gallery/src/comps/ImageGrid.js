b28cc2c5b36bb59df212c631b413b80a8b9344ed
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2022-08-18 18:19:00 -0400

    added css and modal

--- photo-gallery/src/comps/ImageGrid.js
+++ photo-gallery/src/comps/ImageGrid.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import useFirstore  from '../hooks/useFirestore'
 
-const ImageGrid = () => {
+const ImageGrid = ({setSelectedImage}) => {
     const { docs } = useFirstore
     ('images');
     console.log(docs);
 
     return(
         <div className='img-grid'>
-            {docs && docs.map( doc => (<div className='img-wrap' key={doc.id}>
+            {docs && docs.map( doc => (<div className='img-wrap' key={doc.id}
+            onClick = {() => setSelectedImage(doc.url)}>
                 <img src={doc.url} alt='image'/>
             </div>))}
         </div>
--- photo-gallery/src/comps/Title.js
+++ photo-gallery/src/comps/Title.js
@@ -4,7 +4,7 @@ const Title = () => {
   return (
     <div className="title">
       <h1>FrontLine</h1>
-      <h2>Your Pictures</h2>
+      <h2>Pictures</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
   )
