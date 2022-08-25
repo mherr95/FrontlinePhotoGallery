564ea80e132b8ca410d22345251d86711e8ceed9
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2022-08-25 00:39:51 -0400

    switch names

--- photo-gallery/src/comps/Title.js
+++ photo-gallery/src/comps/Title.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import  { Link } from 'react-router-dom'; 
 
 const Title = () => {
   return (
-    <div className="title">
-      <h1>
-        {/* <img class='logo' src= 'photos/Frontline.jpeg' alt='frontline' /> */}
-        Us<i class="fa-solid fa-infinity"></i>
-      </h1>
-      <h2>Live, Laugh, Love </h2>
-      <p>
-        "You are my person."
-      </p>
-    </div>
+      <div className='title'>
+            <h1>
+              {/* <img class='logo' src= 'photos/Frontline.jpeg' alt='frontline' /> */}
+              Frontline<i class="fa-solid fa-cross"></i>
+            </h1>
+        <h2>Our Family </h2>
+        <p>
+          "We love him, because he first loved us..." 1 John 4:19
+        </p>
+      </div>
   )
 }
 
